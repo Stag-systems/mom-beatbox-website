43d63b27f7e5dcc35bedcdd6b8cc4e8dd0e749e8
Stag-systems <stag-systems@proton.me>
2026-01-03 16:14:39 +0100

    Fix build and remove PWA setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-if (import.meta.env.DEV && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => registration.unregister())
-  })
-  if ('caches' in window) {
-    caches.keys().then((keys) => keys.forEach((key) => caches.delete(key)))
-  }
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
